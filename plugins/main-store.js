6a1c69d81ef0568da0a70630e51c92aa687bae08
hendrastore <102216067+hendrastore@users.noreply.github.com>
2022-10-03 04:06:34 +0700

    Update main-store.js

--- plugins/main-store.js
+++ plugins/main-store.js
@@ -106,9 +106,9 @@ conn.reply(global.nomorown + '@s.whatsapp.net', m.quoted ? teks + m.quoted.text 
   /* m.reply('☑️ Pesan Telah terkirim ke Owner!\n_*Menunggu confirmasi Dari Owner...*_')*/
 }
 
-handler.help = ['store1']
+handler.help = ['store']
 handler.tags = ['main']
-handler.command = /^(store1)$/i
+handler.command = /^(store)$/i
 
 export default handler
 
