36d4482c949ec7f5f6175b280e76eb2246e734ce
hendrastore <102216067+hendrastore@users.noreply.github.com>
2022-10-05 01:42:54 +0700

    Add files via upload

---
+++ plugins/groupbot.js
@@ -0,0 +1,31 @@
+let handler = async (m, { conn }) => {
+let info = `
+*${htki} GROUP BOT ${htka}*
+`
+const sections = [
+   {
+	title: `𝗝𝗢𝗜𝗡 𝗞𝗘 𝗚𝗥𝗨𝗣 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗕𝗢𝗧*\n\n\n━━━〔 Dᴏɴ'ᴛ ғᴏʀɢᴇᴛ Dᴏɴᴀᴛɪᴏɴ 〕━━━`,
+	rows: [
+	    {title: '💌 › Group GB MLLB', description: "Group Utama Fangz BOT", rowId:".gcbot"},
+        {title: '✉️ › Grup Bot WhatsApp', description: "Group Kedua", rowId:".gcbot2"},
+        {title: '📮 › Donasi', description: "Donasi Untuk Hendra ≧▽≦", rowId:".donasi"},
+	    ]
+      },
+]
+
+const listMessage = {
+  text: ' ',
+  footer: info,
+  title: null,
+  buttonText: "Klik di sini",
+  sections
+}
+await conn.sendMessage(m.chat, listMessage, { quoted: m})
+//conn.sendHydrated(m.chat, info, wm, null, sgc, "🌎 Group Official", null,null, [['Owner','.owner']], m)
+}
+
+handler.help = ['groupbot']
+handler.tags = ['info']
+handler.command = /^(groupbot)$/i
+
+export default handler
