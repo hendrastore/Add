26a9a68240e1b2ea031771e07f572259d0059c7e
hendrastore <102216067+hendrastore@users.noreply.github.com>
2022-10-06 02:04:01 +0700

    Add files via upload

--- plugins/bot.js
+++ plugins/bot.js
@@ -1,17 +1,10 @@
-import fs from'fs'
-let { MessageType } = (await import('@adiwajshing/baileys')).default
 let handler = async (m, { conn }) => {
-let whmods = fs.readFileSync('./mp3/Bot.opus') 
-conn.sendFile(m.chat, whmods, '', '', m, true)
-}
+let caption = `*(￣ヘ￣）ʜᴍ ᴀᴘᴀᴋᴀᴋ*`
 
-handler.customPrefix = /^(bot|robot|p|hallo|hello|tes|tes123|tes1|tes12|woy)$/i
+conn.sendButton( m.chat, caption, `©️ ʜᴇɴᴅʀᴀ-ʙᴏᴛ-ᴍᴅ᭄`, `ᵐᵉⁿᵘ⚠`, `.menu`, m)
+
+       }
+       
+handler.customPrefix = /^(bot|botwa|robotwa|robot)/i
 handler.command = new RegExp
-
-handler.limit = true
-handler.mods = false 
-handler.premium = false
-handler.group = false
-handler.private = false
-
-export default handler
+module.exports = handler
--- plugins/hello.js
+++ plugins/hello.js
@@ -1,20 +1,111 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-let handler  = async (m, { conn, usedPrefix: _p }) => {
-    let info = fs.readFileSync('./mp3/little-girl-sayingtalking-hello-sound-effectcute-sound-effectsadorable.opus')
 
-let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+let moment = require('moment-timezone')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let handler = async (m, {conn}) => {
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+    let d = new Date
+    let date = d.toLocaleDateString('id', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    })
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else who = m.sender
+    let user = global.db.data.users[who]
+let anu = `${ucapan()}
+Halo👋
+Ada Yang Bisa Saya Bantu?
+Tekan *MENU*
 
-conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow",
-    mediaType: 2,
-    description: "https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow", 
-    title: 'whmods-ᴍᴜʟᴛɪᴅᴇᴠɪᴄᴇ',
-    body: wm,
-    thumbnail: thumb,
-    sourceUrl: sig  }}})
+Runtime: ${uptime}`
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           hydratedContentText: anu,
+           locationMessage: { 
+           jpegThumbnail: gambar() }, 
+           hydratedFooterText: `${date}`,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '📍instagram',
+               url: instagram
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: 'Menu',
+               id: '.menu',
+             }
+
+           },
+               {
+             quickReplyButton: {
+               displayText: 'Jangan Di Pencet',
+               id: 'huuu',
+             }
+
+           }]
+         }
+       }
+     }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
 }
-handler.customPrefix = /^(hallo|hello|alo|allo)$/i
+handler.customPrefix = /^bot$/i // ketik bot (tanpa prefix)
 handler.command = new RegExp
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.admin = false
+handler.botAdmin = false
+handler.fail = null
+module.exports = handler
 
-export default handler
+function ucapan() {
+  const time = moment.tz('Asia/Jakarta').format('HH')
+  if (time >= 5) {
+    res = "Selamat pagi🌄"
+  }
+  if (time > 9) {
+    res = "Selamat siang🏞️"
+  }
+  if (time >= 15) {
+    res = "Selamat sore🌇"
+  }
+  if (time >= 19) {
+    res = "Selamat malam🌃"
+  }
+  return res
+}
+function gambar() {
+  const time = moment.tz('Asia/Jakarta').format('HH')
+  if (time >= 5) {
+    res = fs.readFileSync('./media/pagi.png')
+  }
+  if (time > 9) {
+    res = fs.readFileSync('./media/siang.png')
+  }
+  if (time >= 15) {
+    res = fs.readFileSync('./media/sore.png')
+  }
+  if (time >= 19) {
+    res = fs.readFileSync('./media/malam.png')
+  }
+  return res
+}
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
